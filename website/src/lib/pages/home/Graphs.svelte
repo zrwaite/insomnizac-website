5387f04fbd05bb4a80401e1a7f2567b2cfa42785
Zac Waite <129032699zw@gmail.com>
2022-10-09 18:10:40 -0400

    Update code graph links

--- website/src/lib/pages/home/Graphs.svelte
+++ website/src/lib/pages/home/Graphs.svelte
@@ -1,10 +1,10 @@
 <section class="graphsSection">
 	<img width="390" src="https://github-readme-stats.vercel.app/api/top-langs/?username=zrwaite&hide=makefile,powershell,html,css&layout=compact&langs_count=10&theme=dark" alt="Github Most Used Stats"/>
-	<img width="390" src="https://graphs.insomnizac.xyz/api/wakatime?addDefaultIgnore=true&ignore=george&username=Insomnizac" alt="Wakatime Most Used Stats"/>
+	<img width="390" src="https://graphs.insomnizac.xyz/api/wakatime/Insomnizac" alt="Wakatime Most Used Stats"/>
 	<img width="390" src='https://github-readme-stats.vercel.app/api/wakatime?username=insomnizac&layout=compact&theme=dark&langs_count=10&hide=json,markdown,git%20config,bash,%20other,%20xml,yaml' alt='Languages over last 7 days '/>
 	<img width="390" src="https://github-readme-stats.vercel.app/api?username=zrwaite&show_icons=true&line_height=27&count_private=true&title_color=ffffff&text_color=c9cacc&icon_color=2bbc8a&bg_color=1d1f21" alt="Github Stats"/>
 	<img width="390" src='https://github-readme-streak-stats.herokuapp.com/?user=zrwaite&theme=dark' alt='Github Streak Stats' />
-	<img width="800" src="https://graphs.insomnizac.xyz/api/streak?username=zrwaite" alt="Github Streak Graph" />
+	<img width="800" src="https://graphs.insomnizac.xyz/api/streak/zrwaite" alt="Github Streak Graph" />
 </section>
 
 <style lang="scss">

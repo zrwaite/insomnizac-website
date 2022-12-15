9955a5b906c24b36a348a01b4d848b8988112b7f
Zac Waite <129032699zw@gmail.com>
2022-12-15 01:46:50 -0500

    Routing on admin console

--- admin/console/src/app.rs
+++ admin/console/src/app.rs
@@ -1,12 +1,51 @@
-use yew::prelude::*;
+use yew::{Callback, html, Html, function_component};
+use yew_router::{BrowserRouter, Switch, Routable};
+use yew_router::prelude::use_navigator;
+
+#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/secure")]
+    Secure,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+#[function_component(Secure)]
+fn secure() -> Html {
+    let navigator = use_navigator().unwrap();
+    let onclick_callback = Callback::from(move |_| navigator.push(&Route::Home));
+    html! {
+        <div>
+            <h1>{ "Secure" }</h1>
+            <button onclick={onclick_callback}>{ "Go Home" }</button>
+        </div>
+    }
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+fn switch(routes: Route) -> Html {
+    html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            {
+                match routes {
+                    Route::Home => html! { <h1>{ "Home" }</h1> },
+                    Route::Secure => html! {
+                        <Secure />
+                    },
+                    Route::NotFound => html! { <h1>{ "404" }</h1> },
+                }
+            }
         </main>
     }
 }
